e566e8df3e62cec4cfbc0d26946c6b55ee1c855e
kgreen1200 <k_green5@u.pacific.edu>
2022-03-15 15:50:47 -0700

    more linting

--- source/automation_helpers/boilerplate.py
+++ source/automation_helpers/boilerplate.py
@@ -11,7 +11,7 @@ from ctypes import wintypes
 
 import win32con
 
-user32 = ctypes.WinDLL('user32', use_last_error=True)
+user32 = ctypes.WinDLL("user32", use_last_error=True)
 
 # C Struct Definitions
 
@@ -23,18 +23,18 @@ class MouseInput(ctypes.Structure):
         ("mouseData", wintypes.DWORD),
         ("dwFlags", wintypes.DWORD),
         ("time", wintypes.DWORD),
-        ("dwExtraInfo", wintypes.WPARAM)
+        ("dwExtraInfo", wintypes.WPARAM),
     )
 
 
 class KeyboardInput(ctypes.Structure):
     _fields_ = (
         ("wVk", wintypes.WORD),
-            ("wScan", wintypes.WORD),
-            ("dwFlags", wintypes.DWORD),
-            ("time", wintypes.DWORD),
-            ("dwExtraInfo", wintypes.WPARAM)
-        )
+        ("wScan", wintypes.WORD),
+        ("dwFlags", wintypes.DWORD),
+        ("time", wintypes.DWORD),
+        ("dwExtraInfo", wintypes.WPARAM)
+    )
 
     def __init__(self, *args, **kwds):
         super(KeyboardInput, self).__init__(*args, **kwds)
@@ -49,6 +49,7 @@ class HardwareInput(ctypes.Structure):
 class Input(ctypes.Structure):
     class _Input(ctypes.Union):
         _fields_ = (("ki", KeyboardInput), ("mi", MouseInput), ("hi", HardwareInput))
+
     _anonymous_ = ("_input",)
     _fields_ = (("type", wintypes.DWORD), ("_input", _Input))
 
