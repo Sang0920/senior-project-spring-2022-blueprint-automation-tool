7a966e215ad44aa3c8f450792aa229b5934ce451
kgreen1200 <k_green5@u.pacific.edu>
2022-03-21 08:12:08 -0700

    fix: duplicate code

--- tests/unit/test_place_parser.py
+++ tests/unit/test_place_parser.py
@@ -3,20 +3,22 @@ import os
 from source.blueprint_automation_tool import place_parser
 
 
-def test_place_color():
+def get_location():
     parser = place_parser.PlaceParser()
     _here = os.path.dirname(os.path.abspath(__file__))
     file = os.path.join(_here, "Rotunda.kml")
     loc = parser.parse_place(file)[0]
-    assert loc.color == "ffaa00"
+    return loc
+
+
+def test_place_color():
+    assert get_location().color == "ffaa00"
 
 
 def test_get_line_length_bearing():
-    parser = place_parser.PlaceParser()
-    _here = os.path.dirname(os.path.abspath(__file__))
-    file = os.path.join(_here, "Rotunda.kml")
-    loc = parser.parse_place(file)[0]
-    block_loc = parser.get_line_length_bearing(
+    p = place_parser.PlaceParser()
+    loc = get_location()
+    block_loc = p.get_line_length_bearing(
         loc.coordinate_list[0].latitude,
         loc.coordinate_list[4].latitude,
         loc.coordinate_list[0].longitude,
