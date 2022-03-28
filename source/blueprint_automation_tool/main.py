b5527c93e423940e88cb8e6cdf593294f0a0d239
Kevin Green <k_green5@u.pacific.edu>
2022-03-28 13:53:50 -0700

    feat: Added Information Screen

--- source/blueprint_automation_tool/main.py
+++ source/blueprint_automation_tool/main.py
@@ -1,7 +1,10 @@
 import window
 from builder import PlaceBuilder
+from game_automation import AutomationException
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.dialog import MDDialog
 
 
 # Declare Screens
@@ -13,7 +16,13 @@ class Screen2(Screen):
     pass
 
 
+class Screen3(Screen):
+    pass
+
+
 class MainApp(MDApp):
+    dialog = None
+
     def build(self):
         self.app_name = "Blueprint Automation Tool"
 
@@ -25,6 +34,7 @@ class MainApp(MDApp):
         self.sm = ScreenManager()
         self.sm.add_widget(Screen1(name="screen1"))
         self.sm.add_widget(Screen2(name="screen2"))
+        self.sm.add_widget(Screen3(name="screen3"))
 
         return self.sm
 
@@ -34,8 +44,26 @@ class MainApp(MDApp):
     def build_places(self):
         b = PlaceBuilder()
         w = window.WindowHandler()
-        b.build_place()
+        try:
+            b.build_place()
+        except AutomationException as e:
+            print(e.message)
+            self.show_error_dialog(e.message)
         w.set_current_window(w.find_window(self.app_name)[0])
+
+    def show_error_dialog(self, message):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Error",
+                text=f"{message}",
+                buttons=[
+                    MDRaisedButton(text="OK", on_release=self.close_error_dialog),
+                ],
+            )
+        self.dialog.open()
+
+    def close_error_dialog(self, obj):
+        self.dialog.dismiss()
 
 
 MainApp().run()
